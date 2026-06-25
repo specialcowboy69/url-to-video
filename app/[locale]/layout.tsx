c091b018aa832d912d2ac7ffdaef68096d0e9dc2
specialcowboy69 <franciscoestangui@yahoo.es>
2026-06-25 11:16:06 +0200

    Add Google Analytics tag

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, getTranslations, setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
@@ -96,6 +97,18 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-3L59024KPH"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-3L59024KPH');
+        `}
+      </Script>
       <body>
         <NextIntlClientProvider messages={messages}>
           {children}
